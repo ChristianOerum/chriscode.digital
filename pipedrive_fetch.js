37cb2ca2b863b65716c7c0a0ab45aa21e92bc3ef
Christian Ørum <chri@bubbles.dk>
2023-01-09 12:24:46 +0100

    5 første KPI'er

--- pipedrive_fetch.js
+++ pipedrive_fetch.js
@@ -6,6 +6,11 @@ async function pipedriveCall(endpoint) {
 
     let start = 0
     let tempArr = []
+    let MRR = 0
+    let CLV = 0
+    let CLV_count = 0
+    let Opsalg_MRR = 0
+    let LHV = 0
 
 
     while (tempArr.length == start) {
@@ -17,9 +22,37 @@ async function pipedriveCall(endpoint) {
             tempArr.push(...data.data)
             console.log(tempArr)
             start += 500
-            console.log(start)
         })
         }
+
+        for (let i = 0; i < tempArr.length; i++) {
+            //MRR
+            if(tempArr[i].value != 0 && tempArr[i].status == "won" ){
+                MRR += tempArr[i].fc934713f91688605b3c516dc29de0a897a865bf
+            }  
+
+            //CLV + lukkede handler værdi
+            if (tempArr[i].status == "won") {
+                CLV += tempArr[i].value
+                CLV_count += 1
+
+                LHV += tempArr[i].value
+            }
+            
+            //Opsalg MRR
+            if (tempArr[i].status == "won" && tempArr[i]["41bf727b06a597ee86020f7b240b88cc02ec9a74"] == "24" && tempArr[i].first_won_time ==  (new Date()).getMonth() + 1) {
+                Opsalg_MRR += tempArr[i].fc934713f91688605b3c516dc29de0a897a865bf
+            }
+
+        } 
+        console.log("MRR= " + MRR)
+        console.log("ARR= " + (MRR*12))
+        console.log("CLV= " + (CLV/CLV_count))
+        console.log("Opsalg_MRR= " + Opsalg_MRR)
+        console.log("Lukkede handler værdi= " + LHV)
+        console.log("CAC?")
+        console.log("CACPayback?")
+        console.log("CAC/CLV?")
     }
 
     
